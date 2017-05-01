5adac4f1cc517e627dd8b9efe50af5a74ce102fa
nick.gamb <nick.gamb@centrify.com>
2017-05-01 13:34:16 -0600

    Added License

--- Centrify.Samples.DotNet.ConnectorHealth/CheckConnectorHealth.cs
+++ Centrify.Samples.DotNet.ConnectorHealth/CheckConnectorHealth.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/**
+ * Copyright 2016 Centrify Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/*This version of Centrify.Samples.DotNet.ApiLib has been modified from its origional version found here 
+* https://github.com/centrify/centrify-samples-dotnet-cs/tree/master/Centrify.Samples.DotNet.ApiLib to better 
+* fit the needs of the SIEM Utitlity that this version is bundled with. */
+
+using System;
 using System.Collections.Generic;
 using Centrify.Samples.DotNet.ApiLib;
 using System.IO;
